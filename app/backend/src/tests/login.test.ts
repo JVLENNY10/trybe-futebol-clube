8d9d2b65d1c9f240a131d5049fe6d2bc1af30153
João Victor <joao.lenny100@gmail.com>
2022-06-11 20:40:05 -0300

    Testando se email é válido
    - Criando teste em login.tests.ts para verificar se email é válido.

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,16 +7,16 @@ import { app } from '../app';
 import { Response } from 'superagent';
 
 import User from '../database/models/User';
-import { loginRequest, loginResponse, userExpected } from './mock/models/usersMocks';
+import { loginRequest, loginResponse, userExpected, invalidEmail } from './mock/models/usersMocks';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Sucesso em uma requisição do tipo POST para /login', () => {
+describe('Sucesso em requisição do tipo POST para /login', () => {
   let response: Response;
 
-  before(async () => {
+  before(() => {
     sinon.stub(User, 'findOne').resolves(userExpected as User);
   });
 
@@ -31,3 +31,30 @@ describe('Sucesso em uma requisição do tipo POST para /login', () => {
     expect(response.body).to.deep.equal(loginResponse);
   });
 });
+
+describe('Fracasso em requisição do tipo POST para /login', () => {
+  let response: Response;
+
+  before(() => {
+    sinon.stub(User, 'findOne').resolves(userExpected as User);
+  });
+
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+  it('E-Mail inválido: Retorna um status 401(Unauthorized) e uma mensagem de erro', async () => {
+    response = await chai.request(app).post('login').send(invalidEmail);
+
+    expect(response).to.have.status(401);
+    expect(response.body.message).to.deep.equal('Incorrect email or password');
+  });
+});
+
+function before(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
+
+function after(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
