481acff9a477b0340a0bc44325d2c817ee4f9542
João Victor <joao.lenny100@gmail.com>
2022-06-12 19:16:36 -0300

    Users Services
    - Chamando função jwtGenerator;
    - Trabalhando com a função jwtGenerator.

--- app/backend/src/services/users.services.ts
+++ app/backend/src/services/users.services.ts
@@ -1,13 +1,16 @@
 import User from '../database/models/User';
+import jwtGenerator from '../jwt/jwtGenerator';
 import { IUsersFunctions, IUser } from '../interfaces/users.interfaces';
 
 class UsersServices implements IUsersFunctions {
   public loginUser = async (email: string): Promise<null | IUser> => {
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ attributes: { exclude: ['password'] }, where: { email } });
 
     if (user !== null) {
-      const { id, username, role } = user;
-      const result = { id, username, role, email };
+      const { id } = user;
+      const token = jwtGenerator({ id, email });
+      const result = { user, token };
+
       return result as IUser;
     }
 
