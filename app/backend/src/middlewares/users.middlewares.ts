13fa2262f612f92f7ba7ddf4b3e56e5c0c81cb8a
João Victor <joao.lenny100@gmail.com>
2022-06-12 18:41:35 -0300

    Users Middlewares
    - Criando função checkPassword para verificar se o password é válido e se existe.

--- app/backend/src/middlewares/users.middlewares.ts
+++ app/backend/src/middlewares/users.middlewares.ts
@@ -4,14 +4,28 @@ class UsersMiddlewares {
   public checkEmail = (
     async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> => {
       const { email } = req.body;
-      const test = 'testando';
-      console.log(test.includes('z'));
 
       if (!email.includes('@') || !email.includes('.com')) {
         return res.status(401).json({ message: 'Incorrect email or password' });
       }
 
       if (email === undefined) {
+        return res.status(400).json({ message: 'All fields must be filled' });
+      }
+
+      next();
+    }
+  );
+
+  public checkPassword = (
+    async (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> => {
+      const { password } = req.body;
+
+      if (password.length <= 6) {
+        return res.status(401).json({ message: 'Incorrect email or password' });
+      }
+
+      if (password === undefined) {
         return res.status(400).json({ message: 'All fields must be filled' });
       }
 
