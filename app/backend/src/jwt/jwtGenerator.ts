8f622eb57dcfc5f6529552d76978793e6d5a0ee5
João Victor <joao.lenny100@gmail.com>
2022-06-15 15:22:40 -0300

    JWT Generator
    - Corrigindo linha 6 do aquivo.

--- app/backend/src/jwt/jwtGenerator.ts
+++ app/backend/src/jwt/jwtGenerator.ts
@@ -3,7 +3,7 @@ import { sign, Secret } from 'jsonwebtoken';
 
 const jwtConfig = { expiresIn: '1d' };
 
-const password = fs.readFileSync('../../jwt.evaluation.key');
+const password = fs.readFileSync('jwt.evaluation.key', 'utf-8');
 
 const jwtGenerator = (payload = {}): Secret => (
   sign({ data: payload }, password, jwtConfig)
