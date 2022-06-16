112131f4948a722ab444b7ac5f7210760f4e9145
João Victor <joao.lenny100@gmail.com>
2022-06-16 19:59:48 -0300

    Users Services
    - Adicionando role no encoder.

--- app/backend/src/services/users.services.ts
+++ app/backend/src/services/users.services.ts
@@ -1,9 +1,15 @@
 import Bcrypt = require('bcryptjs');
+import JwtServices from './jwt.services';
 import User from '../database/models/User';
-import jwtGenerator from '../jwt/jwtGenerator';
 import { IUsersFunctions, IUser } from '../interfaces/users.interfaces';
 
 class UsersServices implements IUsersFunctions {
+  private jwtServices: JwtServices;
+
+  constructor() {
+    this.jwtServices = new JwtServices();
+  }
+
   public login = async (email: string, password: string): Promise<IUser | null> => {
     const user = await User.findOne({ where: { email } });
 
@@ -12,7 +18,7 @@ class UsersServices implements IUsersFunctions {
 
       if (passwordExists) {
         const { id, username, role } = user;
-        const token = jwtGenerator({ id, email });
+        const token = this.jwtServices.encoder({ id, email, role });
 
         const result = {
           user: { id, username, role, email },
@@ -24,6 +30,11 @@ class UsersServices implements IUsersFunctions {
     }
 
     return null;
+  };
+
+  public loginValidate = async (id: number): Promise<string> => {
+    const user = await User.findOne({ where: { id } });
+    return user?.role as string;
   };
 }
 
