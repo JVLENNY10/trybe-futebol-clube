aa652ea117dcb04eaacc049b29add0d15e595632
João Victor <joao.lenny100@gmail.com>
2022-06-16 19:59:12 -0300

    Users Controllers
    - Criando controller loginValidate.

--- app/backend/src/controllers/users.controllers.ts
+++ app/backend/src/controllers/users.controllers.ts
@@ -1,17 +1,28 @@
 import { Request, Response } from 'express';
-import UsersService from '../services/users.services';
+import JwtServices from '../services/jwt.services';
+import UsersServices from '../services/users.services';
+import { IUserToken } from '../interfaces/users.interfaces';
 
 class UsersControllers {
-  private service: UsersService;
+  private usersServices: UsersServices;
+  private jwtServices: JwtServices;
 
   constructor() {
-    this.service = new UsersService();
+    this.usersServices = new UsersServices();
+    this.jwtServices = new JwtServices();
   }
 
   public login = async (req: Request, res: Response): Promise<Response> => {
     const { email, password } = req.body;
-    const user = await this.service.login(email, password);
+    const user = await this.usersServices.login(email, password);
     return res.status(200).json(user);
+  };
+
+  public loginValidate = async (req: Request, res: Response): Promise<Response> => {
+    const token = req.headers.authorization;
+    const { data: { role } } = this.jwtServices.decoder(token as string) as IUserToken;
+
+    return res.status(200).json(role);
   };
 }
 
