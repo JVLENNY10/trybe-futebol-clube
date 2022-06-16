a7cfc7539d9e9c4bf09b9d7f50591fabb158c647
João Victor <joao.lenny100@gmail.com>
2022-06-16 20:00:15 -0300

    Login Router
    - Criando rota para /login/validate

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -6,9 +6,10 @@ const routes = Router();
 const usersControllers = new UsersControllers();
 const usersMiddlewares = new UsersMiddlewares();
 
-const { login } = usersControllers;
+const { login, loginValidate } = usersControllers;
 const { checkBody, checkLogin } = usersMiddlewares;
 
 routes.post('/login', checkBody, checkLogin, login);
+routes.get('/login/validate', loginValidate);
 
 export default routes;
