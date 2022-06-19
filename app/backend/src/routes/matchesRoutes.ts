6748726a685402595a6be875d2fa620316f8511b
João Victor <joao.lenny100@gmail.com>
2022-06-19 16:55:02 -0300

    Matches Routes
    - Criando rota do tipo post para /matches;
    - Adicionando middleware checkToken para rota do tipo post /matches;
    - Adicionando controller create para rota do tipo post /matches.

--- app/backend/src/routes/matchesRoutes.ts
+++ app/backend/src/routes/matchesRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import JwtMiddlewares from '../middlewares/JwtMiddlewares';
 import MatchesControllers from '../controllers/MatchesControllers';
 
 const routes = Router();
+const jwtMiddlewares = new JwtMiddlewares();
 const matchesControllers = new MatchesControllers();
 
-const { getAll, getAllByProgress } = matchesControllers;
+const { checkToken } = jwtMiddlewares;
+const { create, getAll, getAllByProgress } = matchesControllers;
 
 routes.get('/matches', getAllByProgress, getAll);
+routes.post('/matches', checkToken, create);
 
 export default routes;
