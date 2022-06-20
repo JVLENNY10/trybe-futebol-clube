166a4dca4cc71b1c00425237018b95c62488ca4e
João Victor <joao.lenny100@gmail.com>
2022-06-20 16:44:56 -0300

    Matches Routes
    - Adicionando middlewares de validação para a rota do tipo post /mathes.

--- app/backend/src/routes/matchesRoutes.ts
+++ app/backend/src/routes/matchesRoutes.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import JwtMiddlewares from '../middlewares/JwtMiddlewares';
 import MatchesControllers from '../controllers/MatchesControllers';
+import MatchesMiddlewares from '../middlewares/MatchesMiddlewares';
 
 const routes = Router();
 const jwtMiddlewares = new JwtMiddlewares();
 const matchesControllers = new MatchesControllers();
+const matchesMiddlewares = new MatchesMiddlewares();
 
 const { checkToken } = jwtMiddlewares;
+const { checkEqualTeams, checkTeamsExist } = matchesMiddlewares;
 const { getAll, getAllByProgress, matchFinished, matchStarted } = matchesControllers;
 
 routes.get('/matches', getAllByProgress, getAll);
-routes.post('/matches', checkToken, matchStarted);
+routes.post('/matches', checkToken, checkTeamsExist, checkEqualTeams, matchStarted);
 routes.patch('/matches/:id/finish', matchFinished);
 
 export default routes;
