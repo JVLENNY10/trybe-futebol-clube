a751a0aa6f34b752228ff286021f57a8e9bf465e
João Victor <joao.lenny100@gmail.com>
2022-06-20 22:39:38 -0300

    Matches Controllers
    - Alterando nome do método matchFinished para finish;
    - Alterando nome do método matchStarted para start;
    - Criando método update.

--- app/backend/src/controllers/MatchesControllers.ts
+++ app/backend/src/controllers/MatchesControllers.ts
@@ -8,6 +8,12 @@ class MatchesControllers {
     this.services = new MatchesServices();
   }
 
+  public finish = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    await this.services.finish(id);
+    return res.status(200).json({ message: 'Finished' });
+  };
+
   public getAll = async (_req: Request, res: Response): Promise<Response> => {
     const matches = await this.services.getAll();
     return res.status(200).json(matches);
@@ -26,15 +32,16 @@ class MatchesControllers {
     }
   );
 
-  public matchFinished = async (req: Request, res: Response): Promise<Response> => {
-    const { id } = req.params;
-    await this.services.matchFinished(id);
-    return res.status(200).json({ message: 'Finished' });
+  public start = async (req: Request, res: Response): Promise<Response> => {
+    const match = await this.services.start(req.body);
+    return res.status(201).json(match);
   };
 
-  public matchStarted = async (req: Request, res: Response): Promise<Response> => {
-    const match = await this.services.matchStarted(req.body);
-    return res.status(201).json(match);
+  public update = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    const { homeTeamGoals, awayTeamGoals } = req.body;
+    await this.services.update(id, homeTeamGoals, awayTeamGoals);
+    return res.status(200).json({ message: 'Updated Match' });
   };
 }
 
