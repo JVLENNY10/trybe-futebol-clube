8b27096e4d916662106559ecb26754bc42a98369
João Victor <joao.lenny100@gmail.com>
2022-06-23 15:31:05 -0300

    Leaderboard Services
    - Criando método calcTotalPoints;
    - Utilizando método no método getAll.

--- app/backend/src/services/LeaderboardServices.ts
+++ app/backend/src/services/LeaderboardServices.ts
@@ -1,5 +1,6 @@
-import MatchesServices from './MatchesServices';
 import TeamsServices from './TeamsServices';
+import MatchesServices from './MatchesServices';
+import { IMatch } from '../interfaces/MatchesInterfaces';
 
 class LeaderboardServices {
   private matchesServices: MatchesServices;
@@ -10,16 +11,31 @@ class LeaderboardServices {
     this.teamsServices = new TeamsServices();
   }
 
+  private calcTotalPoints = async (teamId: number, match: IMatch) => {
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = match;
+    let points = 0;
+
+    if (teamId === homeTeam && homeTeamGoals > awayTeamGoals) {
+      points += 3;
+    } else if (teamId === awayTeam && homeTeamGoals < awayTeamGoals) {
+      points += 3;
+    } else {
+      points += 1;
+    }
+
+    return points;
+  };
+
   public getAll = async () => {
     const matches = await this.matchesServices.getAll();
     const teams = await this.teamsServices.getAll();
 
-    const leaderboards = matches.map((_match, index) => {
-      const { teamName } = teams[index];
+    const leaderboards = matches.map((match, index) => {
+      const { teamName, id } = teams[index];
 
       return {
         name: teamName,
-        totalPoints: 0,
+        totalPoints: this.calcTotalPoints(id, match),
         totalGames: 0,
         totalVictories: 0,
         totalDraws: 0,
