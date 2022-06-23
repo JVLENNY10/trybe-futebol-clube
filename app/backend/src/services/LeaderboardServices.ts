481e79a5df8af177b2cce409f256a9caaef6bdca
João Victor <joao.lenny100@gmail.com>
2022-06-23 15:17:30 -0300

    Leaderboard Services
    - Apagando método teamsList;
    - Criando método getAll.

--- app/backend/src/services/LeaderboardServices.ts
+++ app/backend/src/services/LeaderboardServices.ts
@@ -1,16 +1,37 @@
+import MatchesServices from './MatchesServices';
 import TeamsServices from './TeamsServices';
 
 class LeaderboardServices {
+  private matchesServices: MatchesServices;
   private teamsServices: TeamsServices;
 
   constructor() {
+    this.matchesServices = new MatchesServices();
     this.teamsServices = new TeamsServices();
   }
 
-  private teamsList = async () => {
+  public getAll = async () => {
+    const matches = await this.matchesServices.getAll();
     const teams = await this.teamsServices.getAll();
-    const teamsIds = teams.filter((team) => team.id);
-    return teamsIds;
+
+    const leaderboards = matches.map((_match, index) => {
+      const { teamName } = teams[index];
+
+      return {
+        name: teamName,
+        totalPoints: 0,
+        totalGames: 0,
+        totalVictories: 0,
+        totalDraws: 0,
+        totalLosses: 0,
+        goalsFavor: 0,
+        goalsOwn: 0,
+        goalsBalance: 0,
+        efficiency: 0,
+      };
+    });
+
+    return leaderboards;
   };
 }
 
