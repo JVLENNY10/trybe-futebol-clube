bf787a11615016b9748267aca0465b5ab261c5aa
João Victor <joao.lenny100@gmail.com>
2022-08-14 21:08:49 -0300

    LeaderboardServices
    - Removendo o método calcGoalsBalance da classe;
    - Removendo o método calcGoalsFavor da classe;
    - Removendo o método calcGoalsOwn da classe;
    - Removendo o método calcTeamUsage da classe;
    - Removendo o método calcTotalDraws da classe;
    - Removendo o método calcTotalGames da classe;
    - Removendo o método calcTotalLosses da classe;
    - Removendo o método calcTotalPoints da classe;
    - Removendo o método calcTotalWins da classe;
    - Refarotando o método getAll da classe;
    - Refarotando o método getAllAndReverseSort da classe.

--- app/backend/src/services/LeaderboardServices.ts
+++ app/backend/src/services/LeaderboardServices.ts
@@ -1,158 +1,42 @@
 import TeamsServices from './TeamsServices';
 import MatchesServices from './MatchesServices';
-import { IMatch } from '../interfaces/MatchesInterfaces';
+import LeaderboardHelpers from '../helpers/LeaderboardHelpers';
 
 class LeaderboardServices {
+  private leaderboardHelpers: LeaderboardHelpers;
   private matchesServices: MatchesServices;
   private teamsServices: TeamsServices;
 
   constructor() {
+    this.leaderboardHelpers = new LeaderboardHelpers();
     this.matchesServices = new MatchesServices();
     this.teamsServices = new TeamsServices();
   }
 
-  private calcGoalsBalance = (newMatches: IMatch[]) => {
-    const goalsFavor = this.calcGoalsFavor(newMatches);
-    const calcGoalsOwn = this.calcGoalsOwn(newMatches);
-
-    return goalsFavor - calcGoalsOwn;
-  };
-
-  private calcGoalsFavor = (newMatches: IMatch[]) => {
-    let goals = 0;
-
-    newMatches.forEach((match) => {
-      const { homeTeamGoals, inProgress } = match;
-
-      if (inProgress === false && homeTeamGoals) {
-        goals += homeTeamGoals;
-      }
-    });
-
-    return goals;
-  };
-
-  private calcGoalsOwn = (newMatches: IMatch[]) => {
-    let goals = 0;
-
-    newMatches.forEach((match) => {
-      const { awayTeamGoals, inProgress } = match;
-
-      if (inProgress === false && awayTeamGoals) {
-        goals += awayTeamGoals;
-      }
-    });
-
-    return goals;
-  };
-
-  public calcTeamUsage = (newMatches: IMatch[]) => {
-    const totalPoints = this.calcTotalPoints(newMatches);
-    const totalGames = this.calcTotalGames(newMatches);
-    const teamUsage = (totalPoints / (totalGames * 3)) * 100;
-    return Number(teamUsage.toFixed(2));
-  };
-
-  private calcTotalDraws = (newMatches: IMatch[]) => {
-    let draws = 0;
-
-    newMatches.forEach((match) => {
-      const { homeTeamGoals, awayTeamGoals, inProgress } = match;
-
-      if (inProgress === false && homeTeamGoals === awayTeamGoals) {
-        draws += 1;
-      }
-    });
-
-    return draws;
-  };
-
-  private calcTotalGames = (newMatches: IMatch[]) => {
-    let games = 0;
-
-    newMatches.forEach((match) => {
-      const { teamHome: { teamName }, inProgress } = match;
-
-      if (inProgress === false && teamName) {
-        games += 1;
-      }
-    });
-
-    return games;
-  };
-
-  private calcTotalLosses = (newMatches: IMatch[]) => {
-    let losses = 0;
-
-    newMatches.forEach((match) => {
-      const { homeTeamGoals, awayTeamGoals, inProgress } = match;
-
-      if (inProgress === false && homeTeamGoals < awayTeamGoals) {
-        losses += 1;
-      }
-    });
-
-    return losses;
-  };
-
-  private calcTotalPoints = (newMatches: IMatch[]) => {
-    let points = 0;
-
-    newMatches.forEach((match) => {
-      const { homeTeamGoals, awayTeamGoals, inProgress } = match;
-
-      if (inProgress === false) {
-        if (homeTeamGoals > awayTeamGoals) {
-          points += 3;
-        }
-
-        if (awayTeamGoals === homeTeamGoals) {
-          points += 1;
-        }
-      }
-    });
-
-    return points;
-  };
-
-  private calcTotalWins = (newMatches: IMatch[]) => {
-    let wins = 0;
-
-    newMatches.forEach((match) => {
-      const { homeTeamGoals, awayTeamGoals, inProgress } = match;
-
-      if (inProgress === false && homeTeamGoals > awayTeamGoals) {
-        wins += 1;
-      }
-    });
-
-    return wins;
-  };
-
-  public getAll = async () => {
+  public getAll = async (localTeam: string) => {
     const matches = await this.matchesServices.getAll();
     const teams = await this.teamsServices.getAll();
 
-    return teams.map((team) => {
-      const newMatches = matches.filter(({ teamHome }) => team.teamName === teamHome.teamName);
+    return teams.map(({ teamName }) => {
+      const filterMatches = this.leaderboardHelpers.checkFilter(localTeam, matches, teamName);
 
       return {
-        name: team.teamName,
-        totalPoints: this.calcTotalPoints(newMatches),
-        totalGames: this.calcTotalGames(newMatches),
-        totalVictories: this.calcTotalWins(newMatches),
-        totalDraws: this.calcTotalDraws(newMatches),
-        totalLosses: this.calcTotalLosses(newMatches),
-        goalsFavor: this.calcGoalsFavor(newMatches),
-        goalsOwn: this.calcGoalsOwn(newMatches),
-        goalsBalance: this.calcGoalsBalance(newMatches),
-        efficiency: this.calcTeamUsage(newMatches),
+        name: teamName,
+        totalPoints: this.leaderboardHelpers.calcTotalPoints(filterMatches, teamName),
+        totalGames: this.leaderboardHelpers.calcTotalGames(filterMatches, teamName),
+        totalVictories: this.leaderboardHelpers.calcTotalWins(filterMatches, teamName),
+        totalDraws: this.leaderboardHelpers.calcTotalDraws(filterMatches, teamName),
+        totalLosses: this.leaderboardHelpers.calcTotalLosses(filterMatches, teamName),
+        goalsFavor: this.leaderboardHelpers.calcGoals(filterMatches, teamName),
+        goalsOwn: this.leaderboardHelpers.calcGoalsOwn(filterMatches, teamName),
+        goalsBalance: this.leaderboardHelpers.calcGoalsBalance(filterMatches, teamName),
+        efficiency: this.leaderboardHelpers.calcTeamUsage(filterMatches, teamName),
       };
     });
   };
 
-  public getAllAndReverseSort = async () => {
-    const leaderboard = await this.getAll();
+  public getAllAndReverseSort = async (localTeam: string) => {
+    const leaderboard = await this.getAll(localTeam);
 
     return leaderboard.sort((team1, team2) => (
       team2.totalPoints - team1.totalPoints
