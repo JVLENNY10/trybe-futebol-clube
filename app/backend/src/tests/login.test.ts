28cf2b8358af298852b05fdd0343d2db2988f689
João Victor <joao.lenny100@gmail.com>
2022-08-16 00:22:13 -0300

    tests
    - Removendo arquivo change.me.test.ts;
    - Fazendo algumas alterações em login.test.ts.

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -5,7 +5,6 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import { Response } from 'superagent';
-
 import User from '../database/models/User';
 
 import {
@@ -22,7 +21,7 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Sucesso em requisição do tipo POST para /login', () => {
+describe('Sucesso em requisição do tipo POST para "/login"', () => {
   let response: Response;
 
   before(() => {
@@ -34,14 +33,14 @@ describe('Sucesso em requisição do tipo POST para /login', () => {
   });
 
   it('Retorna um status 200(OK) e a resposta esperada', async () => {
-    response = await chai.request(app).post('login').send(loginBody);
+    response = await chai.request(app).post('/login').send(loginBody);
 
     expect(response).to.have.status(200);
     expect(response.body).to.deep.equal(loginResponse);
   });
 });
 
-describe('Fracasso em requisição do tipo POST para /login', () => {
+describe('Fracasso em requisição do tipo POST para "/login"', () => {
   let response: Response;
 
   before(() => {
@@ -52,39 +51,39 @@ describe('Fracasso em requisição do tipo POST para /login', () => {
     (User.findOne as sinon.SinonStub).restore();
   });
 
-  it('E-Mail inválido: Retorna um status 401(Unauthorized) e uma mensagem de erro', async () => {
-    response = await chai.request(app).post('login').send(invalidEmail);
+  it('E-Mail inválido - Retorna um status 401(Unauthorized) e uma mensagem de erro', async () => {
+    response = await chai.request(app).post('/login').send(invalidEmail);
 
     expect(response).to.have.status(401);
     expect(response.body.message).to.deep.equal('Incorrect email or password');
   });
 
-  it('Senha inválida: Retorna um status 401(Unauthorized) e uma mensagem de erro', async () => {
-    response = await chai.request(app).post('login').send(invalidPassword);
+  it('Senha inválida - Retorna um status 401(Unauthorized) e uma mensagem de erro', async () => {
+    response = await chai.request(app).post('/login').send(invalidPassword);
 
     expect(response).to.have.status(401);
     expect(response.body.message).to.deep.equal('Incorrect email or password');
   });
 
-  it('E-Mail nulo: Retorna um status 400(Bad Request) e uma mensagem de erro', async () => {
-    response = await chai.request(app).post('login').send(nullEmail);
+  it('E-Mail nulo - Retorna um status 400(Bad Request) e uma mensagem de erro', async () => {
+    response = await chai.request(app).post('/login').send(nullEmail);
 
     expect(response).to.have.status(400);
     expect(response.body.message).to.deep.equal('All fields must be filled');
   });
 
-  it('Senha nula: Retorna um status 400(Bad Request) e uma mensagem de erro', async () => {
-    response = await chai.request(app).post('login').send(nullPassword);
+  it('Senha nula - Retorna um status 400(Bad Request) e uma mensagem de erro', async () => {
+    response = await chai.request(app).post('/login').send(nullPassword);
 
     expect(response).to.have.status(400);
     expect(response.body.message).to.deep.equal('All fields must be filled');
   });
 });
 
-function before(arg0: () => void) {
+const before = (arg0: () => void) => {
   throw new Error('Function not implemented.');
 }
 
-function after(arg0: () => void) {
+const after = (arg0: () => void) => {
   throw new Error('Function not implemented.');
 }
